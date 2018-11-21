007f22074592379df7c84f629c3aed15d962f796
Alfred <angch@tertiaryinfotech.com>
2018-11-21 23:12:33 +0800

    1112

--- exercises/Module 5 Templates/mysite/blog/views.py
+++ exercises/Module 5 Templates/mysite/blog/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 from .models import Post
 
+
 def home(request):
 	posts = Post.objects.all()
-    return render(request, 'home.html', {'posts': posts})
+	return render(request,'home.html',{'posts':posts})
 
-def post_detail(request, id):
-    try:
-        post = Post.objects.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404('Post not found')
-    return render(request, 'post_detail.html', {'post': post}
-
+def post_detail(request,id):
+	try:
+		post = Post.objects.get(id=id)
+	except Post.DoesNotExist:
+		raise Http404('Post not found')
+	return render(request,'post_detail.html',{'post':post})
 
--- exercises/Module 5 Templates/mysite/mysite/urls.py
+++ exercises/Module 5 Templates/mysite/mysite/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.conf.urls import url
 
@@ -8,6 +7,5 @@ from blog import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
-    url(r'^post/(\d+)/', views.post_detail, name='post_detail'),
+    url(r'^post/(\d+)/', views.post_detail, name='post_detail')
 ]
-
